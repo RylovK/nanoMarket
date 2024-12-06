9515e3435e276fedb8bac7ba79cbfd526ae4d74c
krylov <rylov.k@gmail.com>
2024-12-06 14:59:40 +0400

    Eureka implemented. Entity graph for subcategories

--- ProductService/src/main/java/my/project/productservice/repository/CategoryRepository.java
+++ ProductService/src/main/java/my/project/productservice/repository/CategoryRepository.java
@@ -1,7 +1,15 @@
 package my.project.productservice.repository;
 
 import my.project.productservice.entity.Category;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    @EntityGraph(type = EntityGraph.EntityGraphType.FETCH, attributePaths = "subcategories")
+    List<Category> findAllByParentIsNull();
+
+    List<Category> findCategoriesByNameContaining(String name);
 }
