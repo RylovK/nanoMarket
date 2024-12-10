dcb42f60ca929127c66805860bb467659eb2081d
rylov <rylov.k@gmail.com>
2024-12-10 21:41:51 +0400

    Dockerfile for ConfigServer

---
+++ ProductService/src/main/java/my/project/productservice/validator/CategoryValidator.java
@@ -0,0 +1,32 @@
+package my.project.productservice.validator;
+
+import lombok.RequiredArgsConstructor;
+import my.project.productservice.dto.CategoryDTO;
+import my.project.productservice.entity.Category;
+import my.project.productservice.repository.CategoryRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class CategoryValidator implements Validator {
+
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz.equals(Category.class);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        CategoryDTO categoryDTO = (CategoryDTO) target;
+        Optional<Category> founded = categoryRepository.findByName(categoryDTO.getName());
+        if (founded.isPresent()) {
+            errors.rejectValue("name", "duplicate", "Category name already exists");
+        }
+    }
+}
