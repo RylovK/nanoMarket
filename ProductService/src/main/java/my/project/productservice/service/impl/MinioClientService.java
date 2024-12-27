9145e3c3ab052b07811880896abfc536c323e1c8
KRylov <rylov.k@gmail.com>
2024-12-28 01:37:03 +0500

    Fix [minio communication]

--- ProductService/src/main/java/my/project/productservice/service/impl/MinioClientService.java
+++ ProductService/src/main/java/my/project/productservice/service/impl/MinioClientService.java
@@ -16,24 +16,13 @@ import software.amazon.awssdk.services.s3.model.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.UUID;
-
 @Service
 @Slf4j
 public class MinioClientService implements FileUploadService {
 
-    //    @Value("${minio.access-key}")
-//    private String accessKey;
-//
-//    @Value("${minio.secret-key}")
-//    private String secretKey;
-//
-//    @Value("${minio.endpoint}")
     private final String endpoint;
-//
-//    @Value("${minio.region}")
-//    private String region;
-
     private final S3Client s3Client;
 
     public MinioClientService(
@@ -41,32 +30,31 @@ public class MinioClientService implements FileUploadService {
             @Value("${minio.secret-key}") String secretKey,
             @Value("${minio.endpoint}") String endpoint,
             @Value("${minio.region}") String region) {
+        this.endpoint = endpoint;
         this.s3Client = S3Client.builder()
                 .endpointOverride(URI.create(endpoint))
                 .credentialsProvider(() -> AwsBasicCredentials.create(accessKey, secretKey))
                 .region(Region.of(region))
                 .build();
-        this.endpoint = endpoint;
     }
 
     @Override
     public String uploadFile(MultipartFile file, String bucketName) {
         createBucketIfNotExists(bucketName);
         String uniqueFileName = UUID.randomUUID() + "-" + file.getOriginalFilename();
-        InputStream inputStream = null;
-        try {
-            inputStream = file.getInputStream();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        PutObjectRequest request = PutObjectRequest.builder()
-                .bucket(bucketName)
-                .key(uniqueFileName)
-                .build();
 
-        s3Client.putObject(request, RequestBody.fromInputStream(inputStream, file.getSize()));
-        log.info("File {} uploaded successfully", file.getOriginalFilename());
-        return endpoint + "/" + bucketName + "/" + uniqueFileName;
+        try (InputStream inputStream = file.getInputStream()) {
+            PutObjectRequest request = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(uniqueFileName)
+                    .build();
+
+            s3Client.putObject(request, RequestBody.fromInputStream(inputStream, file.getSize()));
+            log.info("File {} uploaded successfully to bucket {}", file.getOriginalFilename(), bucketName);
+            return new URI(endpoint + "/" + bucketName + "/" + uniqueFileName).toString();
+        } catch (IOException | URISyntaxException e) {
+            throw new RuntimeException("Failed to upload file", e);
+        }
     }
 
     private void createBucketIfNotExists(String bucketName) {
@@ -76,15 +64,18 @@ public class MinioClientService implements FileUploadService {
                     .build();
 
             s3Client.headBucket(headBucketRequest);
+            log.info("Bucket {} exists", bucketName);
         } catch (NoSuchBucketException e) {
+            log.info("Bucket {} does not exist. Creating it now.", bucketName);
             CreateBucketRequest createBucketRequest = CreateBucketRequest.builder()
                     .bucket(bucketName)
                     .build();
 
             s3Client.createBucket(createBucketRequest);
-            log.info("Bucket created: {}", bucketName);
+            log.info("Bucket {} created successfully", bucketName);
         } catch (S3Exception e) {
-            throw new RuntimeException(e.getMessage(), e);
+            log.error("Error interacting with S3: {}", e.awsErrorDetails().errorMessage());
+            throw new RuntimeException("Error interacting with S3", e);
         }
     }
 }
